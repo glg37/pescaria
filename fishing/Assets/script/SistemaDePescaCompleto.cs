7c1476492ec8d48e785b8ce38c8ac00a0eb3fbd4
glg37 <bielgutjahr1@gmail.com>
2025-07-10 18:24:20 -0300

    acabei a animação

--- fishing/Assets/script/SistemaDePescaCompleto.cs
+++ fishing/Assets/script/SistemaDePescaCompleto.cs
@@ -1,33 +1,46 @@
 using UnityEngine;
 
-public class SistemaDePescaSimplificado : MonoBehaviour
+public class SistemaDePescaCompleto : MonoBehaviour
 {
     public GanchoController ganchoController;
+    public Animator animatorPersonagem;
+    public float tempoDaAnimacao = 1.2f; // ajuste se necessário
+
+    private bool minigameEmProgresso = false;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !minigameEmProgresso)
         {
-            IniciarMinigame();
+            minigameEmProgresso = true;
+            JogarLinha();
         }
+    }
+
+    void JogarLinha()
+    {
+        if (animatorPersonagem != null)
+        {
+            animatorPersonagem.SetTrigger("Pescar");
+        }
+
+        Invoke(nameof(IniciarMinigame), tempoDaAnimacao);
     }
 
     void IniciarMinigame()
     {
         if (ganchoController != null)
         {
-            
             if (ganchoController.cameraToFollow != null)
             {
                 Vector3 target = ganchoController.transform.position + ganchoController.offsetGancho;
                 ganchoController.cameraToFollow.position = target;
             }
 
-           
             ganchoController.IniciarMinigameComCallback(() =>
             {
                 Debug.Log("Minigame finalizado!");
-               
+                minigameEmProgresso = false;
             });
         }
     }
